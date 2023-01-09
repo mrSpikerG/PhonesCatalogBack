0e5f4b03ca39d20a51c53af10b137bfde7ead89b
thespiker <Fili_vw23@student.itstep.org>
2023-01-09 19:02:48 +0200

    Добавьте файлы проекта.

---
+++ Phones/Controllers/Context/PhoneCatalogContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Phones.Models.Database;
+
+namespace Phones.Controllers.Context;
+
+public partial class PhoneCatalogContext : DbContext
+{
+    public PhoneCatalogContext()
+    {
+    }
+
+    public PhoneCatalogContext(DbContextOptions<PhoneCatalogContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Phone> Phones { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-2I5L44I\\SQLEXPRESS;Initial Catalog=PhoneCatalog;Integrated security=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Phone>(entity =>
+        {
+            entity.HasKey(e => e.PhoneId).HasName("PK__Phone__F3EE4BB05E258382");
+
+            entity.ToTable("Phone");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("money");
+            entity.Property(e => e.PriceType)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Phones/Models/Database/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Phones.Models.Database;
+
+public partial class Phone
+{
+    public int PhoneId { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public string? PriceType { get; set; }
+}
